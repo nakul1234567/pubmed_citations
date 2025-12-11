2a5f8c1e4514f15522048c18bca40b1d3f2518c6
nakul1234567 <nakulbhalla3@gmail.com>
2025-12-10 23:11:23 -0500

    Created file structure

---
+++ template.sh
@@ -0,0 +1,10 @@
+mkdir -p src
+mkdir -p data
+mkdir -p research
+touch src/__init__.py
+touch src/helper.py
+touch requirements.txt
+touch research/trials.ipynb
+touch app.py
+
+echo "files creates"
